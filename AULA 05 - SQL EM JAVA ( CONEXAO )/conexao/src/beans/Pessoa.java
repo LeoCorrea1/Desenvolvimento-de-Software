40822425edc39a9209f45942d2696c421dd1fb3f
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2025-08-26 10:51:03 -0300

    Add files via upload

---
+++ AULA 05 - SQL EM JAVA ( CONEXAO )/conexao/src/beans/Pessoa.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package beans;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Pessoa {
+    private int id;
+    private String Nome;
+    private String sexo;
+    private String idioma;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+    
+    
+    
+}
---
+++ AULA 05 - SQL EM JAVA ( CONEXAO )/conexao/src/conexao/Conexao.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Conexao {
+    public Connection getConexao(){
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/BDAula01?useTimezone=true&serverTimezone=UTC","root","laboratorio");
+            System.out.println("Conexao realizada com sucesso! ");
+            return conn;
+        }
+        catch(Exception e){
+            System.out.println("Erro ao conectar ao BD "+e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
