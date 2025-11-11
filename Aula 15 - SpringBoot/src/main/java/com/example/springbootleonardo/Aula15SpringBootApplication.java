2fa23649e3e3fa668707d2e2cfd2ee62c7a91320
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2025-11-11 09:38:36 -0300

    Add files via upload

---
+++ Aula 15 - SpringBoot/src/main/java/com/example/springbootleonardo/Aula15SpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootleonardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula15SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula15SpringBootApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
